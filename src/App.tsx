e20c4f6a210b540d2fe92e16f46fcca2989f8c32
Gilnei aquino <gilnei.aquino@gmail.com>
2023-08-31 17:06:37 -0300

    inclusão do menu base com nav do bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 import AlunoForm from './app/alunos/AlunoForm';
 import AlunoList from './app/alunos/AlunoList';
-
+import Menu from './app/menu/Menu';
 
 function App() {
   return (
 <div>
+      <Menu/>
       <AlunoForm />
       <AlunoList />
     </div>
