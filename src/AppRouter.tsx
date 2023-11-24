1889d8d25996b0c6a5aee41b1172cec3dc3e6ddb
Gilnei aquino <gilnei.aquino@gmail.com>
2023-11-24 15:45:39 -0300

    acesso noa autorizado

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,5 @@
-// AppRouter.js
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import PerguntasForm from './app/perguntas/PerguntasForm';
 import Cursos from './app/Curso/Cursos';
 import Certificado from './app/Curso/Certificado';
@@ -10,30 +9,56 @@ import AlterarSenha from './app/Login/AlterarSenha';
 import CursoForm from './app/Curso/CursoForm';
 import UsuarioForm from './app/Usuario/UsuarioForm';
 import UsuarioConsultaList from './app/Usuario/UsuarioConsultaList';
-import TokenInvalido from "./app/Login/TokenInvalido";
-import RecuperarSenha from "./app/Login/RecuperarSenha";
+import TokenInvalido from './app/Login/TokenInvalido';
+import RecuperarSenha from './app/Login/RecuperarSenha';
+import autenticacaoController from "./app/Jwt/AutenticacaoController";
+import AcessoNaoAutorizado from "./app/Login/AcessoNaoAutorizado";
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/perguntas-form" element={<PerguntasForm />} />
-        <Route path="/cursos" element={<Cursos />} />
-        <Route path="/curso-form" element={<CursoForm />} />        
-        <Route path="/curso-form/:id" element={<CursoForm />} />
-        <Route path="/certificado" element={<Certificado />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/esqueci-minha-senha" element={<EsqueciMinhaSenha />} />
-        <Route path="/alterar-minha-senha" element={<AlterarSenha />} />
-        <Route path="/recuperar-minha-senha" element={<RecuperarSenha />} />
-        <Route path="/usuario-form" element={<UsuarioForm />} />
-        <Route path="/usuario-form/:id" element={<UsuarioForm />} />
-        <Route path="/usuario-list-consulta" element={<UsuarioConsultaList />} />
-        <Route path="/token-invalido" element={<TokenInvalido />} />
-      </Routes>
-    </Router>
-  );
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const token = autenticacaoController.getToken();
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={isAuthenticated ? <Navigate to="/cursos" /> : <Login />}
+                />
+                <Route path="/cursos" element={<Cursos />} />
+                {/* Outras rotas públicas */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/token-invalido" element={<TokenInvalido />} />
+                <Route path="/esqueci-minha-senha" element={<EsqueciMinhaSenha />} />
+                <Route path="/recuperar-minha-senha" element={<RecuperarSenha />} />
+                <Route path="/alterar-minha-senha" element={<AlterarSenha />} />
+                <Route path="/acesso-nao-autorizado" element={<AcessoNaoAutorizado />} />
+
+                {/* Rotas privadas protegidas */}
+                {isAuthenticated && (
+                    <>
+                        <Route path="/perguntas-form" element={<PerguntasForm />} />
+                        <Route path="/curso-form" element={<CursoForm />} />
+                        <Route path="/curso-form/:id" element={<CursoForm />} />
+                        <Route path="/certificado" element={<Certificado />} />
+                        <Route path="/usuario-form" element={<UsuarioForm />} />
+                        <Route path="/usuario-form/:id" element={<UsuarioForm />} />
+                        <Route
+                            path="/usuario-list-consulta"
+                            element={<UsuarioConsultaList />}
+                        />
+                    </>
+                )}
+            </Routes>
+
+        </Router>
+    );
 };
 
 export default AppRouter;
---
+++ src/app/Login/AcessoNaoAutorizado.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AcessoNaoAutorizado: React.FC = () => {
+    return (
+        <div>
+            <h1>Acesso Não Autorizado</h1>
+            <p>Você não tem permissão para acessar esta página.</p>
+        </div>
+    );
+};
+
+export default AcessoNaoAutorizado;
